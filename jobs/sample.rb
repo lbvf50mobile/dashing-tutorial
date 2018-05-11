29760cf257b38f2b5ca10673d99be22c58e34444
Dmitry <lbvf50.mobile@gmail.com>
2018-05-11 16:50:56 +0700

    Add DashApp into the job

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,11 +1,16 @@
-current_wiether_salt = 0
-current_wiether_nsk = 0
+salt = DashApp::Weather.new("salt")
+nsk = DashApp::Weather.new("nsk")
+
+salt.update
+nsk.update
+current_wiether_salt = salt.temperature
+current_wiether_nsk = nsk.temperature
 
 SCHEDULER.every '2s' do
   last_wiether_salt = current_wiether_salt
-  last_wiether_nsk     = current_wiether_nsk
-  current_wiether_salt = rand(20)
-  current_wiether_nsk     = rand(20)
+  last_wiether_nsk = current_wiether_nsk
+  current_wiether_salt = salt.temperature
+  current_wiether_nsk  = nsk.temperature
 
   # send_event('valuation', { current: current_valuation, last: last_valuation })
   # send_event('karma', { current: current_karma, last: last_karma })
