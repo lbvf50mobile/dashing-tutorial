e440f65491c63404ccdb8729841f312897fd4606
Dmitry <lbvf50.mobile@gmail.com>
2018-05-11 21:28:55 +0700

    Now  using Novosibirsk and Salt Lake City in Widgets

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,19 +1,17 @@
-salt = DashApp::Weather.new("salt")
-nsk = DashApp::Weather.new("nsk")
+salt = DashApp::Weather.new("Salt Lake City")
+nsk = DashApp::Weather.new("Novosibirsk")
 
-salt.update
-nsk.update
+
 current_wiether_salt = salt.temperature
 current_wiether_nsk = nsk.temperature
 
-SCHEDULER.every '2s' do
+SCHEDULER.every '30s' do
+  salt.update
+  nsk.update
   last_wiether_salt = current_wiether_salt
   last_wiether_nsk = current_wiether_nsk
   current_wiether_salt = salt.temperature
   current_wiether_nsk  = nsk.temperature
-
-  # send_event('valuation', { current: current_valuation, last: last_valuation })
-  # send_event('karma', { current: current_karma, last: last_karma })
   send_event('wiether_salt',   { current: current_wiether_salt, last: last_wiether_salt })
   send_event('wiether_nsk',   { current: current_wiether_nsk, last: last_wiether_nsk})
 end
