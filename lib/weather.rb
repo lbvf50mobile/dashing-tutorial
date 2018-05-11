175cbc29d6cd54423aec106dc9e5a73273744986
Dmitry <lbvf50.mobile@gmail.com>
2018-05-11 21:23:34 +0700

    Now get wiether in Novosibirsk and Salt Lake City

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,12 +1,14 @@
 module DashApp
     require "net/http"
     require "uri"
+    require "json"
     class Weather
         def initialize location
             @location = location
             @appid = ENV['WEATHER_TOKEN']
             @uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=#{@location}&APPID=#{@appid}")
             @http = Net::HTTP.new(@uri.host, @uri.port)
+            update
         end
         def update
             @response = @http.request(Net::HTTP::Get.new(@uri.request_uri))
@@ -14,8 +16,14 @@ module DashApp
         def response
             @response
         end
+        def body
+            JSON.parse(@response.body)
+        end
+        def code
+            @response.code.to_i
+        end
         def temperature
-            rand(35)
+            body['main']['temp']
         end
     end
 end
--- test/weather_test.rb
+++ test/weather_test.rb
@@ -1,4 +1,5 @@
 require "./test/test_helper.rb"
+require "pp"
 p ENV['WEATHER_TOKEN']
 p ENV['SOMETHING_THAT_DOSE_NOT_EXSITS_HERE_2388_YES']
 describe DashApp::Weather do
@@ -7,7 +8,6 @@ describe DashApp::Weather do
         @w = DashApp::Weather.new(location)
     end
     it "must return digit" do
-        @w.update
         assert @w.temperature.is_a?(Numeric)
     end
     it "WEAHTER_TOKEN must be in env" do
@@ -15,10 +15,20 @@ describe DashApp::Weather do
         assert_nil ENV['SOMETHING_THAT_DOSE_NOT_EXSITS_HERE_2388_YES']
     end
     it "Should have a responce and it must be 200" do
-        @w.update
         # https://ruby-doc.org/stdlib-2.1.1/libdoc/net/http/rdoc/Net/HTTP.html
         assert @w.response
         assert_equal "200", @w.response.code
+    end
+    it "shold have a body represented as hash and code a as a digit" do
+        pp @w.body
+        assert_equal 200, @w.code
+        assert  @w.body.is_a?(Hash)
+    end 
+    it "Should use Novosibirst and Salt Lake city" do
+        @n = DashApp::Weather.new("Novosibirsk")
+        @s = DashApp::Weather.new("Salt Lake City")
+        assert_equal "200", @n.response.code
+        assert_equal "200", @s.response.code
     end
     
 end
