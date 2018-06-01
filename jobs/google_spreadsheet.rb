064a5d26763811e1d6a5acae6d8790cc7a88a023
Dmitry <lbvf50.mobile@gmail.com>
2018-06-01 20:45:30 +0700

    add job fro spreadsheet

--- jobs/google_spreadsheet.rb
+++ jobs/google_spreadsheet.rb
@@ -2,6 +2,9 @@
 require "google_drive"
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
-
-  send_event('google_spreadsheet', { value: 111  })
+    @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
+    @spreadsheet ||= @session.spreadsheet_by_title("wg001")
+    @worksheet ||= @spreadsheet.worksheets.first
+    @val ||= @worksheet[1,1]
+  send_event('google_spreadsheet', { value: @val  })
 end
