d91ded5a760df1c122408dd416766bba93389807
Dmitry <lbvf50.mobile@gmail.com>
2018-06-01 23:24:14 +0700

    try to work wit get sell

--- getcell.rb
+++ getcell.rb
@@ -1,8 +1,8 @@
 
 require "google_drive"
 @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
-@spreadsheet ||= @session.spreadsheet_by_title("wg001")
+@spreadsheet ||= @session.spreadsheet_by_title("KUHL Dashboards")
 @worksheet ||= @spreadsheet.worksheets.first
 p @worksheet
-p @worksheet[1,1]
+p @worksheet[4,2]
 
