9e83a7253643b897f3061049edb06e683c834a94
Dmitry <lbvf50.mobile@gmail.com>
2018-06-01 20:49:51 +0700

    fix val, now val will change

--- jobs/google_spreadsheet.rb
+++ jobs/google_spreadsheet.rb
@@ -5,6 +5,6 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
     @spreadsheet ||= @session.spreadsheet_by_title("wg001")
     @worksheet ||= @spreadsheet.worksheets.first
-    @val ||= @worksheet[1,1]
+    @val = @worksheet[1,1]
   send_event('google_spreadsheet', { value: @val  })
 end
