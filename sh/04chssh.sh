c591a5c2a505142ebbea5f86eac08ffcba38abdf
Dmitry <lbvf50.mobile@gmail.com>
2018-06-02 02:01:40 +0700

    fix config

--- sh/04chssh.sh
+++ sh/04chssh.sh
@@ -1,11 +1,14 @@
 # change SSH adding new lines to it.
 CONF="/etc/ssh/sshd_config"
 sudo chmod a+w /etc/ssh/sshd_config; ls -alF /etc/ssh/sshd_config
+
 sudo echo "RSAAuthentication yes" >> $CONF
 sudo echo "PubkeyAuthentication yes" >> $CONF
+sudo echo "AuthorizedKeysFile      %h/.ssh/authorized_keys" >> $CONF
+
 sudo chmod a-w /etc/ssh/sshd_config; ls -alF /etc/ssh/sshd_config
 sudo chmod u+w /etc/ssh/sshd_config; ls -alF /etc/ssh/sshd_config
-sudo echo "AuthorizedKeysFile      %h/.ssh/authorized_keys" >> $CONF
+
 sudo echo echo $(cat .ssh/id_rsa.pub) > .ssh/authorized_keys
 [ -f .ssh/authorized_keys ] && echo "1 - OK " || "1 - Fails"
 cat .ssh/authorized_keys
