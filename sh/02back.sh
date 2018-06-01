33af877b33d85435a32b7de14b6974934ec27abc
Dmitry <lbvf50.mobile@gmail.com>
2018-06-01 23:57:44 +0700

    fix test in 02 bahs

--- sh/02back.sh
+++ sh/02back.sh
@@ -3,6 +3,6 @@ BACK=$CONF.fd20180601
 echo "1) Create backup of the $CONF"
 sudo cp $CONF $BACK
 sudo chmod a-w $BACK
-[ -f $BACK] && echo "1 - OK " || "1 - Fails"
+[ -f $BACK ] && echo "1 - OK " || "1 - Fails"
 echo "2) Get host name"
 hostname -I
