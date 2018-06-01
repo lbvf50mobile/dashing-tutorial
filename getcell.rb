65e432ed90539d57b852c7d0719ae4032e505778
Dmitry <lbvf50.mobile@gmail.com>
2018-06-01 23:29:54 +0700

    add some changes

--- getcell.rb
+++ getcell.rb
@@ -1,8 +1,8 @@
 
 require "google_drive"
 @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
-@spreadsheet ||= @session.spreadsheet_by_title("KUHL Dashboards")
-@worksheet ||= @spreadsheet.worksheets.first
+@spreadsheet = @session.spreadsheet_by_title("KUHL Dashboards")
+@worksheet = @spreadsheet.worksheets.first
 p @worksheet
 p @worksheet[4,2]
 
--- jobs/google_spreadsheet.rb
+++ jobs/google_spreadsheet.rb
@@ -3,8 +3,8 @@ require "google_drive"
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
     @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
-    @spreadsheet ||= @session.spreadsheet_by_title("wg001")
-    @worksheet ||= @spreadsheet.worksheets.first
-    @val = @worksheet[1,1]
+    @spreadsheet = @session.spreadsheet_by_title("KUHL Dashboards")
+    @worksheet = @spreadsheet.worksheets.first
+    @val = @worksheet[4,2]
   send_event('google_spreadsheet', { value: @val  })
 end
