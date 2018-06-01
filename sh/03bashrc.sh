afa77ec91fa715e17b4e3e662115b3edc3ea858d
Dmitry <lbvf50.mobile@gmail.com>
2018-06-02 00:48:45 +0700

    Add file to copy the factory defaults

---
+++ sh/03bashrc.sh
@@ -0,0 +1,7 @@
+# copy bash.rc
+CONF="~/.bashrc"
+BACK=$CONF.fd20180601
+echo "1) Create backup of the $CONF"
+sudo cp $CONF $BACK
+sudo chmod a-w $BACK
+[ -f $BACK ] && echo "1 - OK " || "1 - Fails"
