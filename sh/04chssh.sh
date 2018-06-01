e11ba40c5788c530cfeb8fec6163b38df5965deb
Dmitry <lbvf50.mobile@gmail.com>
2018-06-02 02:55:11 +0700

    fix echo

--- sh/04chssh.sh
+++ sh/04chssh.sh
@@ -14,7 +14,7 @@ sudo echo "AuthorizedKeysFile      %h/.ssh/authorized_keys" >> $CONF
 sudo chmod a-w /etc/ssh/sshd_config; ls -alF /etc/ssh/sshd_config
 sudo chmod u+w /etc/ssh/sshd_config; ls -alF /etc/ssh/sshd_config
 
-sudo echo echo $(cat $KEY) > .ssh/authorized_keys
+sudo cp $KEY .ssh/authorized_keys
 [ -f .ssh/authorized_keys ] && echo "1 - OK " || "1 - Fails"
 cat .ssh/authorized_keys
 ls -alF .ssh | grep keys
