c1705a52b98c20750eac2329b66bfd7ba98ec469
Dmitry <lbvf50.mobile@gmail.com>
2018-06-11 17:00:16 +0700

    fix all dolar sign

--- sh/08backak.sh
+++ sh/08backak.sh
@@ -3,7 +3,7 @@ config='/root/.ssh/authorized_keys'
 copy=$config".fd.1"
 
 [ -f $config ] && echo "Exists" || echo "Not Exists"
-cp config $copy
+cp $config $copy
 chmod a-w $copy
 [ -f $copy ] && echo "Succesfully copied" || echo "Not copied at all"
 ls -alF $copy
