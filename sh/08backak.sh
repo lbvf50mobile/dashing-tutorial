93e4107bf4c08c46f1d48591fef4c7968a883aa4
Dmitry <lbvf50.mobile@gmail.com>
2018-06-11 16:59:30 +0700

    Find where the error located

--- sh/08backak.sh
+++ sh/08backak.sh
@@ -2,8 +2,8 @@
 config='/root/.ssh/authorized_keys'
 copy=$config".fd.1"
 
-[ -f config ] && echo "Exists" || echo "Not Exists"
-cp config copy
-chmod a-w copy
-[ -f copy ] && echo "Succesfully copied" || echo "Not copied at all"
-ls -alF copy
+[ -f $config ] && echo "Exists" || echo "Not Exists"
+cp config $copy
+chmod a-w $copy
+[ -f $copy ] && echo "Succesfully copied" || echo "Not copied at all"
+ls -alF $copy
