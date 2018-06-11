af32ec7f2b322b40e81c61c673e20b74e32c3d0a
Dmitry <lbvf50.mobile@gmail.com>
2018-06-11 16:27:18 +0700

    and one more change at the 08backak

--- sh/08backak.sh
+++ sh/08backak.sh
@@ -2,6 +2,7 @@
 config='~/.ssh/authorized_keys'
 copy=$config".fd.1"
 
+[ -f config ] && echo "Exists" || echo "Not Exists"
 cp config copy
 chmod a-w copy
 [ -f copy ] && echo "Succesfully copied" || echo "Not copied at all"
