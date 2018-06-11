6bee50b831002c38ace0ed010eef6a007cc714c1
Dmitry <lbvf50.mobile@gmail.com>
2018-06-11 19:22:49 +0700

    add conditions in script

--- sh/10createsshdir.sh
+++ sh/10createsshdir.sh
@@ -2,11 +2,19 @@
 home='/home/board/'
 ssh=$home".ssh/"
 keys=$ssh"authorized_keys"
-mkdir $ssh
-touch $keys
-chomd a-rwx $ssh
+if [ ! -d $ssh]; then
+    mkdir $ssh
+    else
+    echo $ssh" dir exists"
+fi
+if [ ! -f $keys ]; then
+    touch $keys
+    else
+    echo $keys" file exists"
+fi
+chmod a-rwx $ssh
 chmod u+rwx $ssh
-chomd a-rwx $keys
+chmod a-rwx $keys
 chmod u+rw $keys
 [ -d $ssh ] && echo "SSH dir exists" || echo "SSH dir dosenot exists"
 [ -f $keys ] && echo "keys exists" || echo "keys dose not exists"
