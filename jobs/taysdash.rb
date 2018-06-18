1930de46c444665cc26da84ebec4ec4810cd6e37
Dmitry <lbvf50.mobile@gmail.com>
2018-06-18 23:37:13 +0700

    Add first verson of taysdash

---
+++ jobs/taysdash.rb
@@ -0,0 +1,18 @@
+
+require "google_drive"
+
+rows= %w{water_drank pull_ups squats hip_flexor_stretch push_ups words_written}
+y_offset=2 # row offset
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
+    @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
+    @spreadsheet = @session.spreadsheet_by_title("Tays Dash")
+    @worksheet = @spreadsheet.worksheets.first
+
+    rows.each_with_index{|name,row|
+    left =  @worksheet[row+y_offset,3]
+    send_event(name, { current: left, last: left})
+  }
+   
+  
+end
