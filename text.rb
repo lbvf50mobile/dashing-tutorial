faccb41354a3cd3292a432e9c3ecfbdd16a3a664
Dmitry <lbvf50.mobile@gmail.com>
2018-06-18 23:41:34 +0700

    fix layout

--- text.rb
+++ text.rb
@@ -1,12 +1,14 @@
 require "google_drive"
-rows= %w{water_drank pull_ups squats hip_flexor_stretch push_ups words_written}
 
-y_offset=2 # row ofset
+rows= %w{water_drank pull_ups squats hip_flexor_stretch push_ups words_written}
+y_offset=2 # row offset
+
 @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
 @spreadsheet = @session.spreadsheet_by_title("Tays Dash")
 @worksheet = @spreadsheet.worksheets.first
 
 rows.each_with_index{|name,row|
     left =  @worksheet[row+y_offset,3]
-    p [name,left]
+    done = @worksheet[row+y_offset,2]
+    p [name,done,left]
 }
