bdb0885e98e4129636334efac8c02a49b8af88e1
Dmitry <lbvf50.mobile@gmail.com>
2018-06-19 00:07:52 +0700

    add text .rb

--- text.rb
+++ text.rb
@@ -10,8 +10,9 @@ y_offset=2 # row offset
 
 rows.each_with_index{|name,row|
 left =  @worksheet[row+y_offset,3]
-done = @worksheet[row+y_offset,2]
-done = done.empty? ? 0 : done.to_i
-left = left.empty? ? 0 : left.to_i
-    p [name,done,left,done+left]
+    done = @worksheet[row+y_offset,2]
+    done = done.empty? ? 0 : done.to_i
+    left = left.empty? ? 0 : left.to_i
+    val = (done * 100) / (done + left)
+    p val
 }
