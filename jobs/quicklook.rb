b8e95672739db4038439fe8189850066aea7dc98
Dmitry <lbvf50.mobile@gmail.com>
2018-06-26 00:10:59 +0700

    Add all widjetst

--- jobs/quicklook.rb
+++ jobs/quicklook.rb
@@ -1,13 +1,14 @@
 require "google_drive"
-
-rows_= %w{water_drank pull_ups squats hip_flexor_stretch push_ups words_written}
-y_offset=2 # row offset
-
 SCHEDULER.every '1m', :first_in => 0 do |job|
     @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
     @spreadsheet = @session.spreadsheet_by_title("KUHL Dashboards")
     @worksheet = @spreadsheet.worksheets.first
   
     send_event('visitors',   { info: @worksheet[4,3], persent: @worksheet[5,3]})
-  
+    send_event('sales',   { info: @worksheet[4,5], persent: @worksheet[5,5]})
+    send_event('revenue',   { info: @worksheet[4,7], persent: @worksheet[5,7]})
+
+    send_event('visitors1',   { info: @worksheet[12,3], persent: @worksheet[13,3]})
+    send_event('sales1',   { info: @worksheet[12,5], persent: @worksheet[13,5]})
+    send_event('revenue1',   { info: @worksheet[12,7], persent: @worksheet[13,7]})
 end
