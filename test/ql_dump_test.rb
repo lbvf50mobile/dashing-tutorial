4a23972ed1d09d5660f2a8abe54953999a92b29f
Dmitry <lbvf50.mobile@gmail.com>
2018-06-29 03:10:51 +0700

    Put test with dumps in separate file

---
+++ test/ql_dump_test.rb
@@ -0,0 +1,30 @@
+describe DashApp::QL::VisitorsDump do
+    before do
+        @w = DashApp::QL::VisitorsDump.new
+    end
+    it "instance must answer on w[3,3] == V" do
+        row = 3
+        column = 3
+        assert ?V == @w[row,column]
+    end
+end
+describe DashApp::QL::ProductDump do
+    before do
+        @w = DashApp::QL::ProductDump.new
+    end
+    it "instance must answer on w[3,5] == V" do
+        row = 3
+        column = 5
+        assert ?P == @w[row,column]
+    end
+end
+describe DashApp::QL::RevenueDump do
+    before do
+        @w = DashApp::QL::RevenueDump.new
+    end
+    it "instance must answer on w[3,7] == V" do
+        row = 3
+        column = 7
+        assert ?R == @w[row,column]
+    end
+end
--- test/ql_test.rb
+++ test/ql_test.rb
@@ -1,32 +1,3 @@
 require "./test/ql_dump.rb"
+require "./test/ql_dump_test.rb"
 
-describe DashApp::QL::VisitorsDump do
-    before do
-        @w = DashApp::QL::VisitorsDump.new
-    end
-    it "instance must answer on w[3,3] == V" do
-        row = 3
-        column = 3
-        assert ?V == @w[row,column]
-    end
-end
-describe DashApp::QL::ProductDump do
-    before do
-        @w = DashApp::QL::ProductDump.new
-    end
-    it "instance must answer on w[3,5] == V" do
-        row = 3
-        column = 5
-        assert ?P == @w[row,column]
-    end
-end
-describe DashApp::QL::RevenueDump do
-    before do
-        @w = DashApp::QL::RevenueDump.new
-    end
-    it "instance must answer on w[3,7] == V" do
-        row = 3
-        column = 7
-        assert ?R == @w[row,column]
-    end
-end
