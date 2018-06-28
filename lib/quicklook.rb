bcc861c00d9c2bfb4c6c5f8fe9e4708234924ce6
Dmitry <lbvf50.mobile@gmail.com>
2018-06-29 01:09:32 +0700

    Now able to get data from widget just setting it's top left ceil

--- lib/quicklook.rb
+++ lib/quicklook.rb
@@ -3,8 +3,21 @@ module DashApp
         def initialize(worksheet)
             @worksheet = worksheet
         end
+        # offset from top left value
+        def map
+            [
+                {name: :title, offset: [0,0]},
+                {name: :value, offset: [1,0]},
+                {name: :persent, offset: [2,0]}
+            ]
+        end
         def to_hash(top_left_ceil)
-            {title: ?V, value: 7, persent: 4}
+            map.reduce({}) do |s,i|
+                row =  i[:offset][0]+top_left_ceil[0]
+                column = i[:offset][1]+top_left_ceil[1]
+                s[i[:name]] = @worksheet[row,column]
+                s
+            end
         end
     end
 end
