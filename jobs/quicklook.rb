11aa0cc4b4d59fe9d9692c432187a2674bbfbea7
Dmitry <lbvf50.mobile@gmail.com>
2018-06-29 01:32:54 +0700

    get data from widget using [3,?C]

--- jobs/quicklook.rb
+++ jobs/quicklook.rb
@@ -6,9 +6,9 @@ require "google_drive"
 
 quicklook = DashApp::QuickLook.new(@worksheet)
 
-send_event('visitors',   quicklook.to_hash([3,3]))
-send_event('sales',   { value: @worksheet[4,5], persent: @worksheet[5,5]})
-send_event('revenue',   { value: @worksheet[4,7], persent: @worksheet[5,7]})
+send_event('visitors',   quicklook.to_hash([3,?C]))
+send_event('sales',   quicklook.to_hash([3,?E]))
+send_event('revenue',   quicklook.to_hash([3,?G]))
 
 send_event('visitors1',   { value: @worksheet[12,3], persent: @worksheet[13,3]})
 send_event('sales1',   { value: @worksheet[12,5], persent: @worksheet[13,5]})
