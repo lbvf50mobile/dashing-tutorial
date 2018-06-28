9c5a5d3e388ba2e9a7e99df34c5347a7d90a0402
Dmitry <lbvf50.mobile@gmail.com>
2018-06-29 03:25:39 +0700

    DashApp::QL::Visitors

--- lib/ql.rb
+++ lib/ql.rb
@@ -1,6 +1,14 @@
 module DashApp
     module QL
         class Visitors < QuickLook
+            def map
+                [
+                    {name: :title0, offset: [0,0]},
+                    {name: :value0, offset: [1,0]},
+                    {name: :persent0, offset: [2,0]},
+                    {name: :day0, offset: [3,0]}
+                ]
+            end
         end
     end
 end
