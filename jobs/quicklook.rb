0b08a19953b14bd546f105d48963f13100e7b075
Dmitry <lbvf50.mobile@gmail.com>
2018-06-29 03:40:27 +0700

    add data to top bar

--- jobs/quicklook.rb
+++ jobs/quicklook.rb
@@ -12,6 +12,14 @@ send_event('visitors1',   quicklook.to_hash([11,?C]))
 send_event('sales1',   quicklook.to_hash([11,?E]))
 send_event('revenue1',   quicklook.to_hash([11,?G]))
 
+v = DashApp::QL::Visitors.new(@worksheet)
+p = DashApp::QL::Product.new(@worksheet)
+r = DashApp::QL::Revenue.new(@worksheet)
+ans = v.to_hash([3,"C"])
+ans = ans.merge(p.to_hash([3,"E"]))
+ans = ans.merge(r.to_hash([3,"G"]))
+send_event('ql-top',ans)
+
 SCHEDULER.every '1m', :first_in => 0 do |job|
     @session ||= GoogleDrive::Session.from_service_account_key("wg.json")
     @spreadsheet = @session.spreadsheet_by_title("KUHL Dashboards")
@@ -24,4 +32,12 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     send_event('visitors1',   quicklook.to_hash([11,?C]))
     send_event('sales1',   quicklook.to_hash([11,?E]))
     send_event('revenue1',   quicklook.to_hash([11,?G]))
+
+    v = DashApp::QL::Visitors.new(@worksheet)
+    p = DashApp::QL::Product.new(@worksheet)
+    r = DashApp::QL::Revenue.new(@worksheet)
+    ans = v.to_hash([3,"C"])
+    ans = ans.merge(p.to_hash([3,"E"]))
+    ans = ans.merge(r.to_hash([3,"G"]))
+    send_event('ql-top',ans)
 end
--- test/ql_test.rb
+++ test/ql_test.rb
@@ -3,19 +3,19 @@ require "./test/ql_dump_test.rb"
 
 describe DashApp::QL::Visitors do
     it "Must understand Anderstand A-Z columns" do
-        quicklook = DashApp::QL::Visitors.new(DashApp::QL::VisitorsDump.new)
-        assert_equal({title0: ?V, value0: 7, persent0: 4, day0: 5}, quicklook.to_hash([3,"C"]))
+        v = DashApp::QL::Visitors.new(DashApp::QL::VisitorsDump.new)
+        assert_equal({title0: ?V, value0: 7, persent0: 4, day0: 5}, v.to_hash([3,"C"]))
     end
 end
 describe DashApp::QL::Product do
     it "Must understand Anderstand A-Z columns" do
-        quicklook = DashApp::QL::Product.new(DashApp::QL::ProductDump.new)
-        assert_equal({title1: ?P, value1: 5, persent1: 8, day1: 5}, quicklook.to_hash([3,"E"]))
+        p = DashApp::QL::Product.new(DashApp::QL::ProductDump.new)
+        assert_equal({title1: ?P, value1: 5, persent1: 8, day1: 5}, p.to_hash([3,"E"]))
     end
 end
 describe DashApp::QL::Revenue do
     it "Must understand Anderstand A-Z columns" do
-        quicklook = DashApp::QL::Revenue.new(DashApp::QL::RevenueDump.new)
-        assert_equal({title2: ?R, value2: 4, persent2: 1, day2: 4}, quicklook.to_hash([3,"G"]))
+        r = DashApp::QL::Revenue.new(DashApp::QL::RevenueDump.new)
+        assert_equal({title2: ?R, value2: 4, persent2: 1, day2: 4}, r.to_hash([3,"G"]))
     end
 end
