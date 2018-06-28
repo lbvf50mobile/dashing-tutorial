cecec0076893ac3e14845c879a84450103345ee1
Dmitry <lbvf50.mobile@gmail.com>
2018-06-29 00:42:29 +0700

    Quicklook add to_hash method

--- lib/quicklook.rb
+++ lib/quicklook.rb
@@ -1,4 +1,10 @@
 module DashApp
-    class Quicklook
+    class QuickLook
+        def initialize(worksheet)
+            @worksheet = worksheet
+        end
+        def to_hash(top_left_ceil)
+            Hash.new
+        end
     end
 end
