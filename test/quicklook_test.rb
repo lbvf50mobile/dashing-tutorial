32ade218a51fc8d1a15059948f5359aa1e6aabd1
Dmitry <lbvf50.mobile@gmail.com>
2018-06-29 00:11:21 +0700

    Set set mechanism values for dump

--- test/quicklook_test.rb
+++ test/quicklook_test.rb
@@ -4,12 +4,24 @@ require "pp"
 
 
 describe DashApp::WorkSheetDump do
+    before do
+        @w = DashApp::WorkSheetDump.new
+    end
     it "Must have a working constructor" do
         DashApp::WorkSheetDump.new
    end
     it "instance must answer on w[3,3] == V" do
-        w = DashApp::WorkSheetDump.new
-        assert ?V == w[3,3]
+        row = 3
+        column = 3
+        assert ?V == @w[row,column]
+    end
+    it "shold throw exception when point to incorrect ceils" do
+        # https://stackoverflow.com/a/14428915/8574922
+         assert_raises RuntimeError do
+            row = 1500
+            column = 1500
+            @w[row,column]
+         end
     end
 end
 
--- test/work_sheet_dump.rb
+++ test/work_sheet_dump.rb
@@ -1,8 +1,18 @@
 module DashApp
     class WorkSheetDump
         def initialize
+            @allowed_ceils = allowed_ceils.reduce([]){|s,i| s << i[:row_col]}
+        end
+        def allowed_ceils
+            [
+                {row_col: [3,3], valule: "Visitors"},
+                {row_col: [4,3], valule: 7},
+                {row_col: [5,3], valule: "4%"},
+                
+            ]
         end
         def [](row,column)
+            raise "[#{row},#{column}] Ceil is out of range." unless @allowed_ceils.include?([row,column])
             if 3 == row && 3 == column
                 ?V
             end
