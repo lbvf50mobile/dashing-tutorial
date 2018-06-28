1787d21a7b77df0b66effba9ce43d85102bf895d
Dmitry <lbvf50.mobile@gmail.com>
2018-06-29 00:25:29 +0700

    Now I have woderfull dump class to creating any kind of vidget

--- test/work_sheet_dump.rb
+++ test/work_sheet_dump.rb
@@ -6,17 +6,15 @@ module DashApp
         end
         def allowed_ceils
             [
-                {row_col: [3,3], valule: ?V},
-                {row_col: [4,3], valule: 7},
-                {row_col: [5,3], valule: 4},
+                {row_col: [3,3], value: ?V},
+                {row_col: [4,3], value: 7},
+                {row_col: [5,3], value: 4},
                 
             ]
         end
         def [](row,column)
             raise "[#{row},#{column}] Ceil is out of range." unless @allowed_ceils.include?([row,column])
-            if 3 == row && 3 == column
-                ?V
-            end
+            @values[[row,column]]
         end
     end
 end
